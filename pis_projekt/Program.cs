7d819ea5dd4852a24adff7f98fe5c1ac21be0049
Karla <kdelic01@fesb.hr>
2023-03-15 18:56:09 +0100

    Add project files.

---
+++ pis_projekt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pis_projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
